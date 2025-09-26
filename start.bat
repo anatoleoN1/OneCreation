30ec5e08b5231ffa913a3b0307a6023af701c780
anatoleoN1 <anatole.oerlemans@gmail.com>
2025-09-26 17:28:59 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal
 
-REM Vérifie si Node.js est installé et s'il s'agit de la version 18.20.8
+REM Vérifie si Node.js est installé et s'il s'agit de la version 20.19.5
 for /f "delims=" %%v in ('node -v 2^>nul') do set NODE_VERSION=%%v
 
 if "%NODE_VERSION%"=="" (
-    echo Node.js non trouvé. Téléchargement et installation de Node.js 18.20.8...
-    powershell -Command "Invoke-WebRequest -Uri https://nodejs.org/dist/v18.20.8/node-v18.20.8-x64.msi -OutFile node18.msi"
-    msiexec /i node18.msi /quiet /norestart
-    del node18.msi
+    echo Node.js non trouvé. Téléchargement et installation de Node.js 20.19.5...
+    powershell -Command "Invoke-WebRequest -Uri https://nodejs.org/dist/v20.19.5/node-v20.19.5-x64.msi -OutFile node20.msi"
+    msiexec /i node20.msi /quiet /norestart
+    del node20.msi
 ) else (
     echo Node.js déjà installé : %NODE_VERSION%
 )
