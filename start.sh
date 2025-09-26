832340a4d9ea8b1c1d6c13facf21b78a9c7ad2ff
anatoleoN1 <anatole.oerlemans@gmail.com>
2025-09-26 17:29:21 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Vérifie si Node.js est installé et s'il s'agit de la version 18.20.8
+# Vérifie si Node.js est installé et s'il s'agit de la version 20.19.5
 if ! command -v node &> /dev/null
 then
-    echo "Node.js non trouvé. Installation de Node.js 18.20.8..."
-    curl -O https://nodejs.org/dist/v18.20.8/node-v18.20.8-linux-x64.tar.xz
-    sudo tar -C /usr/local --strip-components=1 -xJf node-v18.20.8-linux-x64.tar.xz
-    rm node-v18.20.8-linux-x64.tar.xz
+    echo "Node.js non trouvé. Installation de Node.js 20.19.5..."
+    curl -O https://nodejs.org/dist/v20.19.5/node-v20.19.5-linux-x64.tar.xz
+    sudo tar -C /usr/local --strip-components=1 -xJf node-v20.19.5-linux-x64.tar.xz
+    rm node-v20.19.5-linux-x64.tar.xz
 else
     NODE_VERSION=$(node -v)
-    if [[ "$NODE_VERSION" != "v18.20.8" ]]; then
-        echo "Version actuelle : $NODE_VERSION. Remplacement par Node.js 18.20.8..."
-        curl -O https://nodejs.org/dist/v18.20.8/node-v18.20.8-linux-x64.tar.xz
-        sudo tar -C /usr/local --strip-components=1 -xJf node-v18.20.8-linux-x64.tar.xz
-        rm node-v18.20.8-linux-x64.tar.xz
+    if [[ "$NODE_VERSION" != "v20.19.5" ]]; then
+        echo "Version actuelle : $NODE_VERSION. Remplacement par Node.js 20.19.5..."
+        curl -O https://nodejs.org/dist/v20.19.5/node-v20.19.5-linux-x64.tar.xz
+        sudo tar -C /usr/local --strip-components=1 -xJf node-v20.19.5-linux-x64.tar.xz
+        rm node-v20.19.5-linux-x64.tar.xz
     fi
 fi
 
