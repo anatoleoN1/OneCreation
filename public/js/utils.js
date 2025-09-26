798e25c9e1d34ae49809b8d28167346b256e06fb
anatoleoN1 <anatole.oerlemans@gmail.com>
2025-09-26 17:35:27 +0200

    Update utils.js

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,8 +1,12 @@
-function randomNoise(x, z) {
+// utils.js
+
+// Bruit pseudo-aléatoire basé sur x et z
+export function randomNoise(x, z) {
   const seed = Math.sin(x * 374761393 + z * 668265263) * 43758.5453;
   return seed - Math.floor(seed);
 }
 
-function key(x,y,z) {
+// Génère une clé unique pour identifier un bloc
+export function key(x, y, z) {
   return `${x},${y},${z}`;
 }
